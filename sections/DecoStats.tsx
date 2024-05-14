071698c3206a9f255019be65733722d01b966941
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-05-14 18:35:38 -0300

    new decostats

--- sections/DecoStats.tsx
+++ sections/DecoStats.tsx
@@ -1,12 +1,11 @@
 export interface Props {
-    name: string;
+  name: string;
 }
 
-export default function(props: Props) {
+export default function (props: Props) {
   return (
     <div>
       <h1>{props.name}</h1>
     </div>
-  )
-
+  );
 }
