549234124ef7670ad11e39a2769935a59ae39c82
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-05-29 23:59:46 -0500

    go

--- sections/RenderTest.tsx
+++ sections/RenderTest.tsx
@@ -1,20 +1,28 @@
 export interface Props {
-  nodes: number,
-  childs: number
+  nodes: number;
+  childs: number;
 }
 
-const Fox = ({ nodes, childs, name }: { nodes: number, childs: number, name: string }) => (
-    <>
-        {childs ? 
-            <div className="fox-container">
-                {[...Array(nodes)].map((_, index) => (
-                    <Fox nodes={nodes} childs={childs - 1} name={`Fox ${name} ${childs} ${index + 1}`} />
-                    ))}
-            </div> : <span>{`${nodes} ${childs} ${name}`}</span>}
-    </>
+const Fox = (
+  { nodes, childs, name }: { nodes: number; childs: number; name: string },
+) => (
+  <>
+    {childs
+      ? (
+        <div className="fox-container">
+          {[...Array(nodes)].map((_, index) => (
+            <Fox
+              nodes={nodes}
+              childs={childs - 1}
+              name={`Fox ${name} ${childs} ${index + 1}`}
+            />
+          ))}
+        </div>
+      )
+      : <span>{`${nodes} ${childs} ${name}`}</span>}
+  </>
 );
 
-
-export function renderExample({nodes, childs}: Props) {
+export function renderExample({ nodes, childs }: Props) {
   return <Fox nodes={nodes} childs={childs} name={Math.random() + ""} />;
 }
