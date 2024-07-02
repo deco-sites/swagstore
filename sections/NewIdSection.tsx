0bd971c915d1a83a4c56b5bfff3c92b5685b306d
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-07-01 21:14:32 -0300

    Lint

--- sections/NewIdSection.tsx
+++ sections/NewIdSection.tsx
@@ -1,15 +1,14 @@
 import { Suggestion } from "apps/commerce/types.ts";
 
-
 interface Props {
   /**
-  * @description The description of name.
-  */
+   * @description The description of name.
+   */
   name?: string;
 
   sug: Suggestion | null;
 }
 
 export default function Section({ sug }: Props) {
-  return <div>Hello {sug.products?.map(x => x.productID).join(" ")}</div>
+  return <div>Hello {sug.products?.map((x) => x.productID).join(" ")}</div>;
 }
--- sections/RenderTest.tsx
+++ sections/RenderTest.tsx
@@ -1,25 +1,34 @@
 export interface Props {
-  nodes: number,
-  childs: number
+  nodes: number;
+  childs: number;
 }
 
-const Fox = ({ nodes, childs, name }: { nodes: number, childs: number, name: string }) => (
-    <>
-        {childs ? 
-            <div className="fox-container">
-                {[...Array(nodes)].map((_, index) => (
-                    <Fox nodes={nodes} childs={childs - 1} name={`Fox ${name} ${childs} ${index + 1}`} />
-                    ))}
-            </div> : <span>{`${nodes} ${childs} ${name}`}</span>}
-    </>
+const Fox = (
+  { nodes, childs, name }: { nodes: number; childs: number; name: string },
+) => (
+  <>
+    {childs
+      ? (
+        <div className="fox-container">
+          {[...Array(nodes)].map((_, index) => (
+            <Fox
+              nodes={nodes}
+              childs={childs - 1}
+              name={`Fox ${name} ${childs} ${index + 1}`}
+            />
+          ))}
+        </div>
+      )
+      : <span>{`${nodes} ${childs} ${name}`}</span>}
+  </>
 );
 
 export async function loader(props: Props, req: Request) {
   const sleep = parseInt((new URL(req.url)).searchParams.get("sleep") ?? "1");
-  await new Promise(resolve => setTimeout(resolve, sleep * 1000));
+  await new Promise((resolve) => setTimeout(resolve, sleep * 1000));
   return props;
 }
 
-export default function renderExample({nodes, childs}: Props) {
+export default function renderExample({ nodes, childs }: Props) {
   return <Fox nodes={nodes} childs={childs} name={Math.random() + ""} />;
 }
