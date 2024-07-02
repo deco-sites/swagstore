f844f4de318cfab42fc5c6403faf7e1d4df125a4
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-07-01 21:50:24 -0300

    Noop

--- loaders/Test/noopTest.ts
+++ loaders/Test/noopTest.ts
@@ -1,6 +1,6 @@
 export type Result = { status: string };
 
-export default async function loader(_props: unknown, req: Request): Promise<Result> {
+export default async function loader(_props: unknown, req: Request, _ctx: unknown): Promise<Result> {
     fetch("https://rich-puma-16.deno.dev/noop?N=" + (new URL(req.url)).searchParams.get("N"));
     return {status: "ok"};
 }
