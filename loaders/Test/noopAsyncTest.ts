2e175a711153580d5f0178a87c2b413f31688f7e
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-07-01 21:54:28 -0300

    share type

--- loaders/Test/noopAsyncTest.ts
+++ loaders/Test/noopAsyncTest.ts
@@ -1,4 +1,4 @@
-export type Result = { status: string };
+import { Result } from "./noopTest.ts";
 
 export default async function loader(_props: unknown, req: Request, _ctx: unknown): Promise<Result> {
     await fetch("https://rich-puma-16.deno.dev/noop?N=" + (new URL(req.url)).searchParams.get("N"));
