9995174d70085c218e1fdd1db566aef22d87bc34
Matheus Gaudêncio <matheus@deco.cx>
2024-07-10 17:46:29 -0300

    Release staging environment with 1 changes made by Matheus Gaudêncio

--- loaders/Test/noopAsyncTest.ts
+++ loaders/Test/noopAsyncTest.ts
@@ -1,4 +1,4 @@
-import { Result } from "./noopTest.ts";
+export type Result = { status: string };
 
 export default async function loader(_props: unknown, req: Request, _ctx: unknown): Promise<Result> {
     await fetch("https://rich-puma-16.deno.dev/noop?N=" + (new URL(req.url)).searchParams.get("N"));
